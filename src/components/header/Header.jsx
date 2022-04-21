a73c348697a2e96fe8fcfb06da5f0ed19ea49788
201501101Nicowid <nicolaus.live@gmail.com>
2022-04-21 19:43:21 +0700

    header component jsx, css and responsive design done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./header.css";
-import "../../index.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
@@ -8,7 +7,7 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      <div className="container container__header">
+      <div className="container header__container">
         <h5>Hello i'm</h5>
         <h1>Nicolaus Widi</h1>
         <h5 className="text-light">Fullstack Developer</h5>
