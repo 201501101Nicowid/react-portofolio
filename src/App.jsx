5094fbade2b17492314ff2ce4754db77789b7613
201501101Nicowid <nicolaus.live@gmail.com>
2022-05-03 16:43:59 +0700

    experience added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Experience from "./components/experince/Experience";
+import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portofolio from "./components/portofolio/Portofolio";
 import Testimonials from "./components/testimonials/Testimonials";
